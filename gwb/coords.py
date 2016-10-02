607fb13ed0664328a149a3ab3615d4da574c7141
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-10-02 11:11:12 -0400

    pep8 violations

--- gwb/coords.py
+++ gwb/coords.py
@@ -41,8 +41,8 @@ def get_tangent_basis(ra, dec):
     for cra, cdec in zip(ra, dec):
         s.append(
             np.array([
-                [-np.sin(cra),  np.cos(cra), 0.],
+                [-np.sin(cra), np.cos(cra), 0.],
                 [-np.sin(cdec)*np.cos(cra), -np.sin(cdec)*np.sin(cra), np.cos(cdec)],
-                [np.cos(cdec)*np.cos(cra),  np.cos(cdec)*np.sin(cra), np.sin(cdec)]
-                ]))
+                [np.cos(cdec)*np.cos(cra), np.cos(cdec)*np.sin(cra), np.sin(cdec)]
+            ]))
     return np.array(s).squeeze()
