39eb5d8734cc5a1d2fdcfbdf703c8fdc506e4a06
Semyeong Oh <semyeong.oh@gmail.com>
2016-10-07 13:49:44 -0400

    all from project root

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,11 @@
 
 # takes about 35s to run
 # this will produce 58 pairs
-python scripts/generate-pair-sample.py -o --tgas-file ~/data/gaia/tgas_source/stacked_tgas.fits --snr-cut 80 --output-path . neighbors -n 1 --dv 1
+python scripts/generate-pair-sample.py -o --tgas-file data//stacked_tgas.fits --snr-cut 80 --output-path . neighbors -n 1 --dv 1
 
 echo "pairs file created"
 
 # takes about 2min
-python scripts/compute-likelihood-ratio.py --tgas-file ~/data/gaia/tgas_source/stacked_tgas.fits \
+python scripts/compute-likelihood-ratio.py --tgas-file data/stacked_tgas.fits \
   --pairs-file snr80_n1_dv1.fits \
   --output-path .
