correct distance prior, and parallax covariance

gwb/fml.py
@@ -29,7 +29,7 @@ def ln_parallax_prior(parallax, min_parallax=0.1):
     parallax = np.atleast_1d(parallax)
     lnp = np.full(parallax.shape, -np.inf)
     good_ix = parallax > min_parallax
-    lnp[good_ix] = -4*np.log(parallax[good_ix])
+    lnp[good_ix] = -2*np.log(parallax[good_ix])
     return lnp
 
 def get_posterior_distance_samples(star, size=1, min_parallax=0.1):
@@ -57,7 +57,7 @@ def get_posterior_distance_samples(star, size=1, min_parallax=0.1):
     samples = np.array([])
     iter = 0
     while len(samples) < size or iter == maxiter:
-        batch = np.random.normal(star._data['parallax'], np.sqrt(star.get_cov()[0,0]), size=batch_size)
+        batch = np.random.normal(star._data['parallax'], np.sqrt(star.get_cov()[2,2]), size=batch_size)
         tmps = ln_parallax_prior(batch, min_parallax=min_parallax)
         tmps -= tmps.max()
 