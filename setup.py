bd1c21df5dc4f0548b84aa1f0ce089e8c79d6565
Timothy Morton <tim.morton@gmail.com>
2016-10-28 02:25:22 -0400

    added script to setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
     license="MIT",
     description="Gaia wide binaries.",
     long_description=rd("README.md"),
+    scripts=['scripts/gwb-fit-pair'],
     package_data={"": ["LICENSE"], "gwb": "tests/data/tgas_subset.fits"},
     install_requires=["six", "schwimmbad"],
     include_package_data=True,
