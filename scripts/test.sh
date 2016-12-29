a6d1d94cb75ee1f41bfb157b2d0fcc2aedf8a6ab
Semyeong Oh <semyeong.oh@gmail.com>
2016-12-29 17:33:43 +0900

    tweak for explicit output name

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,12 @@
 
 # takes about 35s to run
 # this will produce 58 pairs
-python scripts/generate-pair-sample.py -o --tgas-file data//stacked_tgas.fits --snr-cut 80 --output-path . neighbors -n 1 --dv 1
+python scripts/generate-pair-sample.py -o --tgas-file data//stacked_tgas.fits --snr-cut 80 \
+  --outname snr80_n1_dv1.fits neighbors -n 1 --dv 1
 
 echo "pairs file created"
 
 # takes about 2min
-python scripts/compute-likelihood-ratio.py --tgas-file data/stacked_tgas.fits \
-  --pairs-file snr80_n1_dv1.fits \
-  --output-path .
+#python scripts/compute-likelihood-ratio.py --tgas-file data/stacked_tgas.fits \
+#  --pairs-file snr80_n1_dv1.fits \
+#  --output-path .
