11c5c40e0d6cb637beb82ff5258dbb4c17bb1a74
Semyeong Oh <semyeong.oh@gmail.com>
2017-04-26 15:22:08 -0400

    clean up source code dir

--- gwb/starmodels/__init__.py
+++
@@ -1,2 +0,0 @@
-from .models import TGASWideBinaryStarModel, TGASStarModel
-from .cfg import STARMODELDIR
--- gwb/starmodels/cfg.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DATADIR = os.getenv('GWBDATA', os.path.expanduser('~/.gwb'))
-STARMODELDIR = os.path.join(DATADIR, 'starmodels')
-TGASFILE = os.path.join(DATADIR, 'TgasSource.h5')
-AVFILE = os.path.join(DATADIR, 'AVs.txt')
-XMATCHFILE = os.path.join(DATADIR, 'isochrone_xmatch.vot')
